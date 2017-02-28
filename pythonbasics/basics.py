9e803bf8c53e19a90b9ee4c6cce2299e504a6bca
user01 <archit.mehra@postgrad.manchester.ac.uk>
2017-02-28 16:06:23 +0000

    first basic python script

---
+++ pythonbasics/basics.py
@@ -0,0 +1,18 @@
+course = 'python' 
+rating = 5
+print course
+print rating 
+
+b = 3
+c = 4
+
+a= (b**2 + c**2 ) **0.5
+
+print a
+
+print type(a)
+print type(b)
+print type(c)
+
+a=int(a)
+print type(a)
