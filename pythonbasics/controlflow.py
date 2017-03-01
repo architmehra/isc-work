a1c8c3651ba3939927715a824b6ca89b1f14e632
user01 <archit.mehra@postgrad.manchester.ac.uk>
2017-03-01 11:47:35 +0000

    all

---
+++ pythonbasics/controlflow.py
@@ -0,0 +1,20 @@
+x = 1
+if x:
+    print x, "is true"
+
+if 22.1: 
+    print "True"
+
+if "Hello":
+    print "True"
+
+if [1,2]:
+    print "True"
+
+if 0:
+    print "True" 
+
+if 0.0:
+    print "True"
+ 
+
---
+++ pythonbasics/python4.py
@@ -0,0 +1,2 @@
+a=str(a)
+print a + "squared equals" + b + "squared" + c + "squared"
